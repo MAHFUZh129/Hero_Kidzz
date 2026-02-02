9cd59984d880a9765a7cfa128b6780b7ee30ebb5
mahfuzh129 <mahfuzh266@gmail.com>
2026-02-02 15:23:49 +0600

    added metadata

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -3,6 +3,40 @@ import CartButton from "@/components/buttons/CartButton";
 import Image from "next/image";
 import { FaStar, FaShoppingCart, FaCheckCircle } from "react-icons/fa";
 
+
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const product = await getSingleProduct(id);
+
+  return {
+    title: product.title,
+    description:
+      product.description.slice(0, 160) ||
+      "Educational toy designed to help kids learn through play.",
+
+    openGraph: {
+      title: product.title,
+      description:
+        "Fun and educational learning toy for kids. Safe, colorful, and engaging.",
+      images: [
+        {
+          url: product.image || "https://i.ibb.co.com/Ld7J2ZYq/image.png",
+          width: 1200,
+          height: 630,
+          alt: product.title,
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: product.title,
+      description: "Fun and educational learning toy for kids.",
+      images: [product.image || "https://i.ibb.co.com/Ld7J2ZYq/image.png"],
+    },
+  };
+}
+
 export default async function ProductDetails({ params }) {
 
     const {id}=await params 
