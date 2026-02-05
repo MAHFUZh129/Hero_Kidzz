e3df756ac6ef2d4b64cedf2262b454c1de93a521
mahfuzh129 <mahfuzh266@gmail.com>
2026-02-05 11:09:49 +0600

    created a new functionality

--- src/action/server/order.js
+++ src/action/server/order.js
@@ -9,11 +9,23 @@ const { dbConnect, collectionName } = require("@/lib/dbConnect")
 
 const orderCollection = dbConnect(collectionName.ORDER)
 
-export const createOrder = async()=>{
+export const createOrder = async(payload)=>{
     const {user}= await getSession(authOptions) || {}
 
     if(!user) return {success:false}
 
-    const result = await getCart()
-    return result
+     const cart = await getCart();
+  if (cart.length == 0) {
+    return { success: false };
+  }
+
+    const newOrder = {
+    createdAt: new Date().toISOString(),
+    items: cart,
+    ...payload,
+    totalPrice,
+  };
+
+  const result = await orderCollection.insertOne(newOrder);
+    return []
 }
